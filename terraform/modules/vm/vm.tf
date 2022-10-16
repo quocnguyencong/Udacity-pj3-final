f3aabf7a9903f9a5931eba4351f4e260f7b7b989
quocnguyencong <hoanglantim542000@gmail.com>
2022-10-17 01:35:08 +0700

    Update vm.tf

--- terraform/modules/vm/vm.tf
+++ terraform/modules/vm/vm.tf
@@ -7,7 +7,7 @@ resource "azurerm_network_interface" "test" {
     name                          = "internal"
     subnet_id                     = "${var.subnet_id}"
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = "${var.public_ip_address_id}"
+    public_ip_address_id          = "${var.public_ip}"
   }
 }
 
@@ -16,11 +16,13 @@ resource "azurerm_linux_virtual_machine" "test" {
   location            = "${var.location}"
   resource_group_name = "${var.resource_group}"
   size                = "Standard_DS2_v2"
-  admin_username      = "${var.admin_username}"
+  #admin_username      = "${var.admin_username}"
+  admin_username      = "adminuser" 
   network_interface_ids = ["${azurerm_network_interface.test.id}"]
   admin_ssh_key {
-    username   = "${var.admin_username}"
-    public_key = "file(\"~/.ssh/id_rsa.pub\")"
+    #username   = "${var.admin_username}"
+    username   = "adminuser"
+    public_key = ""
   }
   os_disk {
     caching           = "ReadWrite"
