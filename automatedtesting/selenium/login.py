62d2ef8eded0906561e07c8724e0490399deccfa
quocnguyencong <QuocNguyenCong@outlook.com>
2022-11-08 01:00:21 +0700

    update login.py

--- automatedtesting/selenium/login.py
+++ automatedtesting/selenium/login.py
@@ -2,19 +2,17 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options as ChromeOptions
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
 import add_remove_from_cart
-from selenium.webdriver.support import expected_conditions as EC
 
 
 # Start the browser and login with standard_user
 def login (user, password):
     print ('Starting the browser...')
     # --uncomment when running in Azure DevOps.
-    # options = ChromeOptions()
-    # options.add_argument("--headless") 
-    # driver = webdriver.Chrome(options=options)
-    driver = webdriver.Chrome()
+    options = ChromeOptions()
+    options.add_argument("--headless") 
+    driver = webdriver.Chrome(options=options)
+    # driver = webdriver.Chrome()
     print ('Browser started successfully. Navigating to the demo page to login.')
     driver.get('https://www.saucedemo.com/')
 
