361b38d2a547fe090ea5940a11dad16632670032
quocnguyencong <hoanglantim542000@gmail.com>
2022-11-10 00:09:32 +0700

    Update main.tf

--- terraform/environments/test/main.tf
+++ terraform/environments/test/main.tf
@@ -7,10 +7,10 @@ provider "azurerm" {
 }
 terraform {
   backend "azurerm" {
-    storage_account_name = "tfstate2221322342"
+    storage_account_name = "tfstate2417926753"
     container_name       = "tfstate"
     key                  = "test.terraform.tfstate"
-    access_key           = "qdLWp3HW5uPBCuGdCdYr5HarigUs4TQY3cMqYJcLSHPJ04Y9sAXI2iLIUaI+mBVTjNnawE6mNzK3+AStakGesw=="
+    access_key           = "4B7CJLr8QYBWgeHqE+uvPGsC/z86C1T7eTfMruNKyJ+dn2SLM/sKHmrcUVs3R3L192wllhZDsSo6+AStwidLPQ=="
   }
 }
 #module "resource_group" {
