fa7b03cb93a001f6329b5085acbe2ce9465aed10
quocnguyencong <QuocNguyenCong@outlook.com>
2022-11-13 00:44:17 +0700

    update tf

--- terraform/modules/appservice/appservice.tf
+++ terraform/modules/appservice/appservice.tf
@@ -7,7 +7,7 @@ resource "azurerm_service_plan" "test" {
 }
 
 resource "azurerm_linux_web_app" "test" {
-  name                = "${var.application_type}-${var.resource_type}"
+  name                = "fakerestapi-quocnc"
   location            = "${var.location}"
   resource_group_name = "${var.resource_group}"
   service_plan_id     = azurerm_service_plan.test.id
