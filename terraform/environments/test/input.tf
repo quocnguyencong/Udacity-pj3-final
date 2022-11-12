40c8f6a6c782d7cc746ae236460bc8131728d55a
quocnguyencong <QuocNguyenCong@outlook.com>
2022-11-12 23:09:59 +0700

    update input.tf

--- terraform/environments/test/input.tf
+++ terraform/environments/test/input.tf
@@ -3,6 +3,7 @@ variable "subscription_id" {}
 variable "client_id" {}
 variable "client_secret" {}
 variable "tenant_id" {}
+variable "resource_group" {}
 
 # Resource Group/Location
 variable "location" {}
